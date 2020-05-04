c40f453c282ae7b5dde10dc079d7d9ef2813262b
henry <tarou.88@gmail.com>
2020-05-04 14:38:37 +0900

    update folder structure, add problems
    update TwoSum

---
+++ problems/01_twosum/main.go
@@ -0,0 +1,31 @@
+/*
+Write a function that takes a non-empty array of distinct integers and an integer representing a target sum.
+If any two numbers in the input array sum up to the target sum, the function should return them in an array, in any order.
+If no two numbers sum up to the target sum, the function should return an empty array.
+You cannot add a single integer to itself to obtain the target sum.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	target := 10
+	array := []int{3, 5, -4, 8, 11, 1, -1, 6}
+	result := TwoNumberSum(array, target)
+	fmt.Println(result)
+}
+
+// TwoNumberSum O(n^2) time | O(1) space.
+func TwoNumberSum(array []int, target int) []int {
+	for i := 0; i < len(array)-1; i++ {
+		firstNum := array[i]
+		for j := i + 1; j < len(array); j++ {
+			secondNum := array[j]
+			if firstNum+secondNum == target {
+				return []int{firstNum, secondNum}
+			}
+		}
+	}
+	return []int{}
+}
