acf1d4724e85b81e49b442d434af9e7bfe18ed66
henry <tarou.88@gmail.com>
2020-05-07 23:28:31 +0900

    update 02 Binary Search Tree

--- problems/02_BST/main.go
+++ problems/02_BST/main.go
@@ -6,6 +6,41 @@ You can assume that there will only be one closest value.
 
 package main
 
-func main() {
+import "math"
 
+// BST struct.
+type BST struct {
+	Value int
+	Left  *BST
+	Right *BST
+}
+
+func main() {
+	FindClosestValue()
+}
+
+// FindClosestValue to target.
+func (tree *BST) FindClosestValue(target int) int {
+	return tree.findClosestValue(target, math.MaxInt32)
+}
+
+func (tree *BST) findClosestValue(target, closest int) int {
+	currentnode := tree
+	for currentnode != nil {
+		if absdiff(target, closest) > absdiff(target, currentnode.Value) {
+			closest = currentnode.Value
+		}
+		if target < currentnode.Value {
+			currentnode = currentnode.Left
+		} else if target > currentnode.Value {
+			currentnode = currentnode.Right
+		}
+		break
+	}
+	return closest
+}
+
+func absdiff(a, b int) int {
+	out := math.Abs(float64(a) - float64(b))
+	return int(out)
 }
