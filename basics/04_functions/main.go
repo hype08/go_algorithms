8d64f201ce3f75fa1200af987f843d9539b4da6d
henry <tarou.88@gmail.com>
2020-05-10 23:10:38 +0900

    update functions

--- basics/04_functions/main.go
+++ basics/04_functions/main.go
@@ -10,7 +10,21 @@ func getSum(num1, num2 int) int {
 	return num1 + num2
 }
 
+func variadicSum(nums ...int) {
+	fmt.Print(nums, "  ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
 func main() {
 	fmt.Println(greeting("Henry"))
 	fmt.Println(getSum(2, 3))
+	variadicSum(1, 2)
+	variadicSum(1, 2, 3)
+
+	nums := []int{1, 2, 3, 4}
+	variadicSum(nums...)
 }
